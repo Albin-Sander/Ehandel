5ba8ae33fb0ee3091a45afc4115dc95e652dbe38
Albin <albinsander17@outlook.com>
2020-02-10 20:20:27 +0100

    You can now see shoppingcart in products

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+//GET Products
 window.onload = async function getProducts() {
   fetch("http://localhost:8000/product", {
     method: "get"
@@ -13,103 +14,137 @@ window.onload = async function getProducts() {
 function createHtml() {
   console.log(dataset);
   for (let i = 0; i < dataset.length; i++) {
-    var model3Fetch = dataset[0];
-    var modelSFetch = dataset[1];
-    var cyberTruckFetch = dataset[2];
+    //Model 3
+    var createDiv = document.createElement("div");
+    createDiv.setAttribute("id", i);
+    document.querySelector(".grid-container").appendChild(createDiv);
+
+    var createModel3Text = document.createElement("p");
+    createModel3Text.innerHTML = dataset[i].Name;
+    document.querySelector(".grid-container").appendChild(createModel3Text);
+
+    var createModel3Img = document.createElement("img");
+    createModel3Img.src = dataset[i].img;
+
+    console.log(dataset[i].img);
+    document.querySelector(".grid-container").appendChild(createModel3Img);
+
+    var createModel3Price = document.createElement("p");
+
+    createModel3Price.innerHTML = dataset[i].Price;
+    document.querySelector(".grid-container").appendChild(createModel3Price);
+
+    var createModel3Btn = document.createElement("button");
+    createModel3Btn.setAttribute("id", "button" + i);
+
+    createModel3Btn.innerHTML = "Add to cart";
+    document.querySelector(".grid-container").appendChild(createModel3Btn);
+
+    console.log(dataset[i].id);
+
+    document.getElementById("button" + i).onclick = function fetchModel3() {
+      fetch("http://localhost:8000/Shoppingcart?id=" + dataset[i].id, {
+        mode: "no-cors",
+        method: "post"
+      });
+
+      alert("Lade till" + dataset[i].Name + " i varukorgen");
+    };
   }
-  console.log(model3Fetch.Name);
-  console.log(modelSFetch.Name);
-  console.log(cyberTruckFetch.Name);
-
-  //Model 3
-  var createModel3Text = document.createElement("p");
-  createModel3Text.setAttribute("id", "model3Text");
-  createModel3Text.innerHTML = dataset[0].Name;
-  document.querySelector(".grid-container").appendChild(createModel3Text);
-
-  var createModel3Img = document.createElement("img");
-  createModel3Img.src = model3Fetch.img;
-  createModel3Img.setAttribute("id", "model3image");
-  console.log(model3Fetch.img);
-  document.querySelector(".grid-container").appendChild(createModel3Img);
-
-  var createModel3Price = document.createElement("p");
-  createModel3Price.setAttribute("id", "model3price");
-  createModel3Price.innerHTML = model3Fetch.Price;
-  document.querySelector(".grid-container").appendChild(createModel3Price);
-
-  var createModel3Btn = document.createElement("button");
-  createModel3Btn.setAttribute("id", "btn1");
-  createModel3Btn.innerHTML = "Add to cart";
-  document.querySelector(".grid-container").appendChild(createModel3Btn);
-
-  //ModelS
-
-  var createModelSText = document.createElement("p");
-  createModelSText.setAttribute("id", "modelsText");
-  createModelSText.innerHTML = modelSFetch.Name;
-  document.querySelector(".grid-container").appendChild(createModelSText);
-
-  var createModelSImg = document.createElement("img");
-  createModelSImg.src = modelSFetch.img;
-  createModelSImg.setAttribute("id", "modelsimage");
-  document.querySelector(".grid-container").appendChild(createModelSImg);
-
-  var createModelSPrice = document.createElement("p");
-  createModelSPrice.setAttribute("id", "modelsprice");
-  createModelSPrice.innerHTML = modelSFetch.Price;
-  document.querySelector(".grid-container").appendChild(createModelSPrice);
-
-  var createModelSBtn = document.createElement("button");
-  createModelSBtn.setAttribute("id", "btn2");
-  createModelSBtn.innerHTML = "Add to cart";
-  document.querySelector(".grid-container").appendChild(createModelSBtn);
-
-  //Cybertruck
-
-  var createCyberText = document.createElement("p");
-  createCyberText.setAttribute("id", "cybertruckText");
-  createCyberText.innerHTML = cyberTruckFetch.Name;
-  document.querySelector(".grid-container").appendChild(createCyberText);
-
-  var createCyberImg = document.createElement("img");
-  createCyberImg.src = cyberTruckFetch.img;
-  createCyberImg.setAttribute("id", "cybertruckimage");
-  document.querySelector(".grid-container").appendChild(createCyberImg);
-
-  console.log(cyberTruckFetch.Price);
-
-  var createCyberPrice = document.createElement("p");
-  createCyberPrice.setAttribute("id", "cybertruckprice");
-  createCyberPrice.innerHTML = cyberTruckFetch.Price;
-  document.querySelector(".grid-container").appendChild(createCyberPrice);
-
-  var createCyberBtn = document.createElement("button");
-  createCyberBtn.setAttribute("id", "btn3");
-  createCyberBtn.innerHTML = "Add to cart";
-  document.querySelector(".grid-container").appendChild(createCyberBtn);
-
-  document.getElementById("btn1").onclick = function fetchModel3() {
-    fetch("http://localhost:8000/Shoppingcart?id=1", {
-      mode: "no-cors",
-      method: "post"
-    });
-    alert("Lade till Tesla Model 3 i varukorgen");
-  };
-
-  document.getElementById("btn2").onclick = function fetchModelS() {
-    fetch("http://localhost:8000/Shoppingcart?id=2", {
-      mode: "no-cors",
-      method: "post"
-    });
-    alert("Lade till Tesla Model S i varukorgen");
-  };
-
-  document.getElementById("btn3").onclick = function fetchCyberTruck() {
-    fetch("http://localhost:8000/Shoppingcart?id=3", {
-      mode: "no-cors",
-      method: "post"
-    });
-    alert("Lade till Tesla Cybertruck i varukorgen");
-  };
 }
+//Get Shoppingcart
+var setdata;
+document.getElementById(
+  "showshopping"
+).onclick = async function fetchShopping() {
+  fetch("http://localhost:8000/Shoppingcart", {
+    method: "get",
+    mode: "cors"
+  })
+    .then(response => response.json())
+    .then(data => (setdata = data))
+    .then(function() {
+      createShoppingList();
+    })
+    .catch(error => error);
+};
+
+function createShoppingList() {
+  for (let i = 0; i < setdata.length; i++) {
+    console.log(setdata[i].Name);
+    let seeShoppingCart = document.createElement("li");
+    seeShoppingCart.innerHTML = setdata[i].Name;
+    document.querySelector(".shoppingcartlist").appendChild(seeShoppingCart);
+  }
+}
+
+//ModelS
+
+/*var createModelSText = document.createElement("p");
+createModelSText.setAttribute("id", "modelsText");
+createModelSText.innerHTML = modelSFetch.Name;
+document.querySelector(".grid-container").appendChild(createModelSText);
+
+var createModelSImg = document.createElement("img");
+createModelSImg.src = modelSFetch.img;
+createModelSImg.setAttribute("id", "modelsimage");
+document.querySelector(".grid-container").appendChild(createModelSImg);
+
+var createModelSPrice = document.createElement("p");
+createModelSPrice.setAttribute("id", "modelsprice");
+createModelSPrice.innerHTML = modelSFetch.Price;
+document.querySelector(".grid-container").appendChild(createModelSPrice);
+
+var createModelSBtn = document.createElement("button");
+createModelSBtn.setAttribute("id", "btn2");
+createModelSBtn.innerHTML = "Add to cart";
+document.querySelector(".grid-container").appendChild(createModelSBtn);
+
+//Cybertruck
+
+var createCyberText = document.createElement("p");
+createCyberText.setAttribute("id", "cybertruckText");
+createCyberText.innerHTML = cyberTruckFetch.Name;
+document.querySelector(".grid-container").appendChild(createCyberText);
+
+var createCyberImg = document.createElement("img");
+createCyberImg.src = cyberTruckFetch.img;
+createCyberImg.setAttribute("id", "cybertruckimage");
+document.querySelector(".grid-container").appendChild(createCyberImg);
+
+console.log(cyberTruckFetch.Price);
+
+var createCyberPrice = document.createElement("p");
+createCyberPrice.setAttribute("id", "cybertruckprice");
+createCyberPrice.innerHTML = cyberTruckFetch.Price;
+document.querySelector(".grid-container").appendChild(createCyberPrice);
+
+var createCyberBtn = document.createElement("button");
+createCyberBtn.setAttribute("id", "btn3");
+createCyberBtn.innerHTML = "Add to cart";
+document.querySelector(".grid-container").appendChild(createCyberBtn);
+
+document.getElementById("btn1").onclick = function fetchModel3() {
+  fetch("http://localhost:8000/Shoppingcart?id=1", {
+    mode: "no-cors",
+    method: "post"
+  });
+  alert("Lade till Tesla Model 3 i varukorgen");
+};
+
+document.getElementById("btn2").onclick = function fetchModelS() {
+  fetch("http://localhost:8000/Shoppingcart?id=2", {
+    mode: "no-cors",
+    method: "post"
+  });
+  alert("Lade till Tesla Model S i varukorgen");
+};
+
+document.getElementById("btn3").onclick = function fetchCyberTruck() {
+  fetch("http://localhost:8000/Shoppingcart?id=3", {
+    mode: "no-cors",
+    method: "post"
+  });
+  alert("Lade till Tesla Cybertruck i varukorgen");
+};
+*/
--- script/shoppingcart.js
+++ script/shoppingcart.js
@@ -1,116 +1,72 @@
-var dataset;
-window.onload = async function getShoppingCart() {
-  fetch("http://localhost:8000/Shoppingcart", {
-    method: "get"
+window.onload = async function getProducts() {
+  fetch("http://localhost:8000/product", {
+    method: "get",
+    mode: "cors"
   })
     .then(response => response.json())
-    .then(data => (dataset = data))
+    .then(data => (setdata = data))
     .then(function() {
-      logData();
+      logProducts();
     })
     .catch(error => error);
 };
 
-function logData() {
-  console.log(dataset);
-  for (var i = 0; i < 1; i++) {
-    var model3Sum = dataset[i];
-  }
-
-  for (var i = 0; i < 1; i++) {
-    var modelSSum = dataset[i];
-  }
-
-  let myJson = JSON.stringify(dataset);
-  console.log(myJson);
-  let resultsId1 = myJson.includes("Tesla Model 3");
-  let resultsId2 = myJson.includes("Tesla Model S");
-  let resultsId3 = myJson.includes("Tesla Cybertruck");
-
-  if (resultsId1 === true) {
-    document.querySelector(".teslaModel3").innerHTML +=
-      "<p id=model3Title>Tesla Model 3</p>";
-
-    document.querySelector(".teslaModel3").innerHTML +=
-      "<img id=TeslaModel3Image src=img/model3.jpg>";
-
-    if (model3Sum.Price == "200 000 kr") {
-      var para = document.createElement("p");
-      para.innerHTML = model3Sum.Price;
-
-      document.querySelector(".teslaModel3").appendChild(para);
-    } else {
-      var para2 = document.createElement("p");
-      para2.innerHTML = "200 000 kr";
-      document.querySelector(".teslaModel3").appendChild(para2);
-    }
-
-    document.querySelector(".teslaModel3").innerHTML +=
-      "<button id=removeModel3>Remove</button>";
-
-    document.getElementById("removeModel3").onclick = function deleteModel3() {
-      fetch("http://localhost:8000/Shoppingcart?id=1", {
-        method: "Delete"
-      });
-
-      if (confirm("Removed Tesla Model 3 from ShoppingCart") == true) {
-        window.location.reload(true);
-      }
-    };
-  }
-
-  if (resultsId2 === true) {
-    document.querySelector(".teslaModelS").innerHTML +=
-      "<p id=modelSTitle>Tesla Model S</p>";
-
-    document.querySelector(".teslaModelS").innerHTML +=
-      "<img id=TeslaModelSImage src=/img/models.jpg>";
-
-    if (modelSSum.Price == "700 000 kr") {
-      var x = document.createElement("p");
-      x.innerHTML = modelSSum.Price;
-
-      document.querySelector(".teslaModelS").appendChild(x);
-    } else {
-      var y = document.createElement("p");
-      y.innerHTML = "200 000 kr";
-      document.querySelector(".teslaModelS").appendChild(y);
-    }
-
-    document.querySelector(".teslaModelS").innerHTML +=
-      "<button id=removeModelS>Remove</button>";
-
-    document.getElementById("removeModelS").onclick = function DeleteModelS() {
-      fetch("http://localhost:8000/Shoppingcart?id=2", {
-        method: "Delete"
-      });
-      alert("Removed Tesla Model S from Shoppingcart");
-    };
-  }
-
-  if (resultsId3 === true) {
-    document.querySelector(".teslaCyberTruck").innerHTML +=
-      "<p id=cyberTitle>Tesla Cybertruck</p>";
-
-    document.querySelector(".teslaCyberTruck").innerHTML +=
-      "<img id=TeslaCyberImage src=/img/cyber.jpg />";
-
-    document.querySelector(".teslaCyberTruck").innerHTML +=
-      "<p id=cyberPrice>Price: 400 000 kr</p>";
-
-    document.querySelector(".teslaCyberTruck").innerHTML +=
-      "<button id=removeCyber>Remove</button>";
-
-    document.getElementById("removeCyber").onclick = function deleteCyber() {
-      fetch("http://localhost:8000/Shoppingcart?id=3", {
-        method: "Delete"
-      });
-      alert("Removed Tesla Cybertruck from Shoppingcart");
-    };
+function logProducts() {
+  console.log(setdata);
+  for (let i = 0; i < setdata.length; i++) {
+    console.log(setdata[i].Price);
   }
 }
 
-/*async function loadProducts() {
-  let data = await getShoppingCart();
-  console.log(data);
-}*/
+window.onload = async function getShoppingCart() {
+  fetch("http://localhost:8000/ShoppingCart", {
+    method: "get",
+    mode: "cors"
+  })
+    .then(response => response.json())
+    .then(data => (dataset = data))
+    .then(function() {
+      createHtml();
+    })
+    .catch(error => error);
+};
+
+function createHtml() {
+  console.log(dataset);
+  for (let i = 0; i < dataset.length; i++) {
+    var createDiv = document.createElement("div");
+    createDiv.setAttribute("id", i);
+    document.querySelector(".grid-container").appendChild(createDiv);
+
+    var createModel3Text = document.createElement("p");
+    createModel3Text.innerHTML = dataset[i].Name;
+    document.querySelector(".grid-container").appendChild(createModel3Text);
+
+    var createModel3Img = document.createElement("img");
+    createModel3Img.src = dataset[i].img;
+
+    console.log(dataset[i].img);
+    document.querySelector(".grid-container").appendChild(createModel3Img);
+
+    var createModel3Price = document.createElement("p");
+
+    createModel3Price.innerHTML = dataset[i].Price;
+    document.querySelector(".grid-container").appendChild(createModel3Price);
+
+    var createModel3Btn = document.createElement("button");
+    createModel3Btn.setAttribute("id", "button" + i);
+
+    createModel3Btn.innerHTML = "Remove from cart";
+    document.querySelector(".grid-container").appendChild(createModel3Btn);
+
+    console.log(dataset[i].id);
+
+    document.getElementById("button" + i).onclick = function removeProduct() {
+      fetch("http://localhost:8000/Shoppingcart?id=" + dataset[i].id, {
+        mode: "cors",
+        method: "delete"
+      });
+      alert("Removed " + dataset[i].Name + " from shoppingcart");
+    };
+  }
+}
